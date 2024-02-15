45513c0ce8c4beee30bcb465fc6f7854ac163887
momenmian <mohammadmomenmian@hotmail.com>
2024-02-15 23:22:05 +0600

    Refactor array.py to include array function and main function

--- array/array.py
+++ array/array.py
@@ -1,5 +1,13 @@
-print("array")
-print("hi")
-print("his")
-print("this")
-print("news")
+def array():
+    arr = []
+    for i in range(int(input())):
+        arr.append(int(input()))
+    return arr
+
+def main():
+    arr = array()
+    for i in arr:
+        print(i)
+
+if __name__ == "__main__":
+    main()
